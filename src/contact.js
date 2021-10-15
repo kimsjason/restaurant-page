9614fb8d49fae8bd62d8521a568a4db881f54d0f
Jason Kim <jason.s.kim95@gmail.com>
2021-10-15 14:04:49 -0400

    Fix innerHTML syntax

--- src/contact.js
+++ src/contact.js
@@ -1,15 +1,15 @@
 const contact = () => {
     const contact = document.createElement('div');
-    contact.innerHTML('Contact Us');
+    contact.innerHTML = 'Contact Us';
     
     const address = document.createElement('div');
-    address.innerHTML('330 Apgujeong-ro, Gangnam-gu, Seoul');
+    address.innerHTML = '330 Apgujeong-ro, Gangnam-gu, Seoul';
     const daysOfOperation = document.createElement('div');
-    daysOfOperation.innerHTML('Tues-Sat');
+    daysOfOperation.innerHTML = 'Tues-Sat';
     const lunchHours = document.createElement('div');
-    lunchHours.innerHTML('12PM - 3PM');
+    lunchHours.innerHTML = '12PM - 3PM';
     const dinnerHours = document.createElement('div');
-    dinnerHours.innerHTML('6PM - 10PM');
+    dinnerHours.innerHTML = '6PM - 10PM';
 }
 
 export { contact };
--- src/menu.js
+++ src/menu.js
@@ -2,20 +2,20 @@ const menu = () => {
     const content = document.querySelector('#content');
 
     const menu = document.createElement('div');
-    menu.innerHTML('menu');
+    menu.innerHTML = 'menu';
 
     // starters
     const starters = document.createElement('div');
-    starters.innerHTML('starters');
+    starters.innerHTML = 'starters';
 
     const arrancini = document.createElement('div');
-    arrancini.innerHTML('sicilian arancini')
+    arrancini.innerHTML = 'sicilian arancini';
     const bruschetta = document.createElement('div');
-    bruschetta.innerHTML('bruschetta w/ tomatoes, burrata, & anchovies');
+    bruschetta.innerHTML = 'bruschetta w/ tomatoes, burrata, & anchovies';
     const octopus = document.createElement('div');
-    octopus.innerHTML('signature octopus in garlic oil');
+    octopus.innerHTML = 'signature octopus in garlic oil';
     const carpaccio = document.createElement('div');
-    carpaccio.innerHTML('grilled wild oyster mushrooms & wagyu carpaccio');
+    carpaccio.innerHTML = 'grilled wild oyster mushrooms & wagyu carpaccio';
     
     starters.appendChild(arrancini);
     starters.appendChild(bruschetta);
@@ -24,28 +24,28 @@ const menu = () => {
 
     // sides
     const sides = document.createElement('div');
-    sides.innerHTML('sides');
+    sides.innerHTML = 'sides';
 
     const vegetablePlate = document.createElement('div');
-    vegetablePlate.innerHTML('vegetable plate');
+    vegetablePlate.innerHTML = 'vegetable plate';
     const truffleFries = document.createElement('div');
-    truffleFries.innerHTML('truffle fries');
+    truffleFries.innerHTML = 'truffle fries';
     
     sides.appendChild(vegetablePlate);
     sides.appendChild(truffleFries);
 
     // pasta and risotto
     const pasta = document.createElement('div');
-    pasta.innerHTML('pasta & risotto');
+    pasta.innerHTML = 'pasta & risotto';
 
     const seafoodSpaghetti = document.createElement('div');
-    seafoodSpaghetti.innerHTML('oil spaghetti w/ fresh seafood medley');
+    seafoodSpaghetti.innerHTML = 'oil spaghetti w/ fresh seafood medley';
     const trufflePasta = document.createElement('div');
-    trufflePasta.innerHTML('paccheri pasta w/ truffle cream sauce')
+    trufflePasta.innerHTML = 'paccheri pasta w/ truffle cream sauce';
     const bolognese = document.createElement('div');
-    bolognese.innerHTML('spaghetti w/ bolognese sauce');
+    bolognese.innerHTML = 'spaghetti w/ bolognese sauce';
     const abaloneRisotto = document.createElement('div');
-    abaloneRisotto.innerHTML('soft steamed abalone risotto');
+    abaloneRisotto.innerHTML = 'soft steamed abalone risotto';
 
     pasta.appendChild(seafoodSpaghetti);
     pasta.appendChild(trufflePasta);
@@ -54,24 +54,24 @@ const menu = () => {
 
     // mains
     const mains = document.createElement('div');
-    mains.innerHTML('mains');
+    mains.innerHTML = 'mains';
 
     const porkBelly = document.createElement('div');
-    porkBelly.innerHTML('grilled iberico pork belly');
+    porkBelly.innerHTML = 'grilled iberico pork belly';
     const steak = document.createElement('div');
-    steak.innerHTML('charcoal grilled beef rib steak');
+    steak.innerHTML = 'charcoal grilled beef rib steak';
 
     mains.appendChild(porkBelly);
     mains.appendChild(steak);
 
     // desserts
     const desserts = document.createElement('div');
-    desserts.innerHTML('desserts');
+    desserts.innerHTML = 'desserts';
 
     const panettone = document.createElement('div');
-    panettone.innerHTML('panettone w/ vanilla ice cream')
+    panettone.innerHTML = 'panettone w/ vanilla ice cream';
     const affogato = document.createElement('div');
-    affogato.innerHTML('affogato');
+    affogato.innerHTML = 'affogato';
 
     desserts.appendChild(panettone);
     desserts.appendChild(affogato);
