afbc3a56e1466d966c9b5c917508073494e865f4
Jason Kim <jason.s.kim95@gmail.com>
2021-10-15 13:46:42 -0400

    Add contact page module

---
+++ src/contact.js
@@ -0,0 +1,15 @@
+const contact = () => {
+    const contact = document.createElement('div');
+    contact.innerHTML('Contact Us');
+    
+    const address = document.createElement('div');
+    address.innerHTML('330 Apgujeong-ro, Gangnam-gu, Seoul');
+    const daysOfOperation = document.createElement('div');
+    daysOfOperation.innerHTML('Tues-Sat');
+    const lunchHours = document.createElement('div');
+    lunchHours.innerHTML('12PM - 3PM');
+    const dinnerHours = document.createElement('div');
+    dinnerHours.innerHTML('6PM - 10PM');
+}
+
+export { contact };
