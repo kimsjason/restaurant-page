d05cd85fb63f997708179df21ae95af257b692e9
Jason Kim <jason.s.kim95@gmail.com>
2021-10-17 18:16:00 -0400

    Update homepage styles

--- src/homepage.js
+++ src/homepage.js
@@ -1,19 +1,30 @@
-import Image from './images/bistro-spark.jpeg';
+import Img01 from './images/img-01.jpg';
+
 
 const homepage = () => {
     const content = document.querySelector('#content');
-    
-    const logo = document.createElement('h1');
-    const image = document.createElement('img');
+
+    const imageContainer = document.createElement('div');
+    const img01 = document.createElement('img');
+    imageContainer.classList.add('image-container');
+    img01.classList.add('img');
+    imageContainer.appendChild(img01);
+
+    const container = document.createElement('div');
+    const about = document.createElement('h2');
     const description = document.createElement('p');
+    container.classList.add('container');
+    about.classList.add('about');
+    container.appendChild(about);
+    container.appendChild(description);
 
-    logo.innerHTML = 'Bistro Spark';
-    image.src = Image;
+    img01.src = Img01;
+
+    about.innerHTML = 'ABOUT SPARK'
     description.innerHTML = "Since 2016, Bistro Spark has brought the best of Italian cuisine to the busy neighborhood of Apgujeong, Seoul. Stepping off the loud and trendy Apgujeong Rodeo Street and into Bistro Spark, diners are transported into a cozy space that evokes the ambiance of Italy's finest restaurants. Chef Shawn Park serves up classic menu items like abalone risotto and wagyu carpaccio that have attracted celebrities and chefs across the world."
-
-    content.appendChild(logo);
-    content.appendChild(image);
-    content.appendChild(description);
+    
+    content.appendChild(container);
+    content.appendChild(imageContainer);
 }
 
 export { homepage };
--- src/index.js
+++ src/index.js
@@ -1,38 +1,56 @@
-import { homepage } from './homepage.js';
+import { homepage } from './homepage';
 import { menu } from './menu';
 import { contact } from './contact';
-
-const body = document.querySelector('body');
-const content = document.querySelector('#content');
+import './main.css';
+import Logo from './images/spark-bistro-logo.jpeg';
 
 // nav bar
-const navbar = document.createElement('nav');
-const homeTab = document.createElement('a');
-const menuTab = document.createElement('a');
-const contactTab = document.createElement('a');
+const nav = () => {
+    const body = document.querySelector('body');
+    const content = document.querySelector('#content');
+    
+    const navbar = document.createElement('nav');
+    const logo = document.createElement('img');
 
-homeTab.innerHTML = 'Home';
-menuTab.innerHTML = 'Menu';
-contactTab.innerHTML = 'Contact';
+    const navlinks = document.createElement('ul');
+    const homeTab = document.createElement('li');
+    const menuTab = document.createElement('li');
+    const contactTab = document.createElement('li');
+    
+    navbar.classList.add('nav-bar');
+    logo.classList.add('logo');
+    navlinks.classList.add('nav-links');
+    homeTab.classList.add('nav-link');
+    menuTab.classList.add('nav-link');
+    contactTab.classList.add('nav-link');
 
-homeTab.addEventListener('click', () => {
-    content.innerHTML = '';
-    homepage();
-})
-menuTab.addEventListener('click', () => {
-    content.innerHTML = '';
-    menu();
-})
-contactTab.addEventListener('click', () => {
-    content.innerHTML = '';
-    contact();
-})
+    logo.src = Logo;
+    homeTab.innerHTML = 'Home';
+    menuTab.innerHTML = 'Menu';
+    contactTab.innerHTML = 'Contact';
+    
+    homeTab.addEventListener('click', () => {
+        content.innerHTML = '';
+        homepage();
+    })
+    menuTab.addEventListener('click', () => {
+        content.innerHTML = '';
+        menu();
+    })
+    contactTab.addEventListener('click', () => {
+        content.innerHTML = '';
+        contact();
+    })
+    
+    navlinks.appendChild(homeTab);
+    navlinks.appendChild(menuTab);
+    navlinks.appendChild(contactTab);
 
+    navbar.appendChild(logo);
+    navbar.appendChild(navlinks);
 
-navbar.appendChild(homeTab);
-navbar.appendChild(menuTab);
-navbar.appendChild(contactTab);
+    body.prepend(navbar);
+}
 
-body.prepend(navbar);
-
+nav();
 homepage();
